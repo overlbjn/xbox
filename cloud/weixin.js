50b26b4583bb46f57f124e8dc735fc8815280352
overlbjn <402309558@qq.com>
2015-03-08 16:24:09 +0800

    2
    2

--- cloud/weixin.js
+++ cloud/weixin.js
@@ -74,7 +74,12 @@ exports.exec = function(params, cb) {
   		console.log('click = ',params.xml.EventKey);
   		if (params.xml.EventKey=='SHOWINFORMATION') {
   			console.log('开始登录...');
-  			cb(null,'ok');
+  			var result = {
+  				xml: {
+  					Content: 'OK'
+  					}
+  				}
+  			cb(null,result);
   			/*
   			var username = params.xml.FromUserName.toString();
   			var password = params.xml.FromUserName.toString();
