4472253e1e9b0838c526721d3f5b12b421a92aa6
overlbjn <402309558@qq.com>
2015-03-08 16:07:54 +0800

    24
    24

--- cloud/weixin.js
+++ cloud/weixin.js
@@ -79,18 +79,18 @@ exports.exec = function(params, cb) {
   			AV.User.logIn(username,password, {
   				success: function(user) {
   					console.log('登录成功！');
-  					var msgtext = '姓名：'+user.get('nickname')+' 性别：'+user.get('sex')+' 国家：'+user.get('country')+' 省份：'+user.get('province')+' 城市：'+user.get('city')+' 语言：'+user.get('language')+' 头像：'+user.get('headimgurl');
+  					/*var msgtext = '姓名：'+user.get('nickname')+' 性别：'+user.get('sex')+' 国家：'+user.get('country')+' 省份：'+user.get('province')+' 城市：'+user.get('city')+' 语言：'+user.get('language')+' 头像：'+user.get('headimgurl');
   					api.sendText(params.xml.FromUserName.toString(),msgtext,function(error,result){
   									console.log('sendtext_error:'+error+'sendtext_result:'+result);	
-  									cb(error,"result");
-  								})
+  									cb(error,result);
+  								})*/
+  					cb(null,'');		
   				},error: function(user, error) {
   					console.log('登录失败！');
   					cb(error);
   				}
   			});
   		}
-  		cb(null,'');
   	}else{
   		cb(null,'');
   	}
